5dd45c24fd309ed0eba7860f89645f78b426bf8d
KUO <drjkuo@gmail.com>
2017-11-24 11:48:38 -0800

    Display transaction content

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 abi = JSON.parse('[{"constant":false,"inputs":[{"name":"amount","type":"uint256"}],"name":"buy","outputs":[{"name":"bought","type":"uint256"},{"name":"","type":"address"}],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"b","type":"bool"}],"name":"getQueue","outputs":[{"components":[{"name":"amount","type":"uint256"},{"name":"price","type":"uint256"},{"name":"user","type":"address"}],"name":"ret","type":"tuple[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"amount","type":"uint256"}],"name":"sell","outputs":[{"name":"sold","type":"uint256"},{"name":"","type":"address"}],"payable":true,"stateMutability":"payable","type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"value","type":"uint256"},{"indexed":false,"name":"amount","type":"uint256"},{"indexed":false,"name":"price","type":"uint256"}],"name":"PriceCalculated","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"length","type":"uint256"}],"name":"showLength","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"s","type":"string"}],"name":"test","type":"event"}]')
 VotingContract = web3.eth.contract(abi);
 // In your nodejs console, execute contractInstance.address to get the address at which the contract is deployed and change the line below to use your deployed address
-contractInstance =  VotingContract.at('0xe2aff68781985a9bf0a726ac7771e388499fc6dd');
+contractInstance =  VotingContract.at('0x7e566b95cfdd9599634f7842d3a35f4a4729f10b');
 candidates = {"0": "candidate-0", "1": "candidate-1", "2": "candidate-2", "3": "candidate-3", "4": "candidate-4"}
 
 // function voteForCandidate() {
@@ -15,11 +15,11 @@ candidates = {"0": "candidate-0", "1": "candidate-1", "2": "candidate-2", "3": "
 // }
 
 function buySolar() {
-  contractInstance.buy(10, {from: web3.eth.accounts[2], value:web3.toWei(10, "ether"), gas:3000000});
+  contractInstance.buy($(".buy [name=solarAmount]").val(), {from: web3.eth.accounts[$(".buy [name=id]").val()], value:web3.toWei($(".buy [name=ether]").val(), "ether"), gas:3000000});
 }
 
 function sellSolar() {
-  contractInstance.sell(10, {from: web3.eth.accounts[1], value:web3.toWei(10, "ether"), gas:3000000});
+  contractInstance.sell($(".sell [name=solarAmount]").val(), {from: web3.eth.accounts[$(".sell [name=id]").val()], value:web3.toWei($(".sell [name=ether]").val(), "ether"), gas:3000000});
 }
 
 function tradeSolar() {
@@ -28,6 +28,17 @@ function tradeSolar() {
 }
 
 
+
+// // get transaction hash
+// web3.eth.getBlock(4).transactions
+// // get transaction content
+// web3.eth.getTransaction
+// var inputData = web3.eth.abi.decodeParameter("uint256", t.input.substring(10));
+// console.dir(inputData);
+// $('#transactions').append('<tr><td>' + t.blockNumber +
+//   '</td><td>' + from +
+//   '</td><td>' + "ApolloTrade" +
+//   '</td><td>sellEnergy(' + inputData[0].toString() + ')</td></tr>');
 
 $(document).ready(function() {
   // candidateNames = Object.keys(candidates);
@@ -44,5 +55,19 @@ $(document).ready(function() {
     $("#" + candidates[name]).html(address);
     let ether = web3.eth.getBalance(address).c[0];
     $("#" + "ethers-" + name).html(ether);
+  }
+
+  $('#transactions').empty();
+  for(let i=0; i<web3.eth.blockNumber; i++) {
+    if (web3.eth.getBlock(i).transactions.length > 0) {
+      let transactionHash = web3.eth.getBlock(i).transactions[0];
+      console.log(transactionHash);
+      let oneTransaction = (web3.eth.getTransaction(transactionHash)); //
+      if (oneTransaction) {
+        // $('#transactions').append('<tr><td>' + oneTransaction.from + '</td><td>' + oneTransaction.to + '</td></tr>');
+        $('#transactions').append('<tr><td>' + JSON.stringify(oneTransaction) + '</td></tr>');
+      }    // console.log(oneTransaction);
+      //
+    }
   }
 });
