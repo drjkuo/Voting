3ce0eb0a4462880e020b76f2881cbd300f08872b
KUO <drjkuo@gmail.com>
2017-11-23 17:04:50 -0800

    update script

--- solarScript.js
+++ solarScript.js
@@ -9,16 +9,12 @@ byteCode = compiledCode.contracts[':Solar'].bytecode;
 deployedContract = SolarContract.new({data: byteCode, from: web3.eth.accounts[0], gas: 4700000});
 contractInstance = SolarContract.at(deployedContract.address);
 
-// > web3.eth.accounts[0]
-// '0x2d869f300b084bf19626423c19b6da8873845a77'
-// > web3.eth.getBalance('0x2d869f300b084bf19626423c19b6da8873845a77')
-// { [String: '99999999999995013137'] s: 1, e: 19, c: [ 999999, 99999995013137 ] }
-// > contractInstance.sell("10", {from: web3.eth.accounts[1], gas:3000000})
-// '0xf7c148635ac2851e05e04f2098b8a7caa0ed5c1011762f76e92329cd708b018f'
-// > contractInstance.buy("10", {from: web3.eth.accounts[2], gas:3000000})
-// '0x0b4839742c79cb33b7adbe3ca46fc54944c8ad1d122b306962f292a8207e1e76'
-// > web3.eth.getBalance(web3.eth.accounts[1])
-// { [String: '99999999999999845999'] s: 1, e: 19, c: [ 999999, 99999999845999 ] }
-// > web3.eth.getBalance(web3.eth.accounts[2])
-// { [String: '99999999999999944217'] s: 1, e: 19, c: [ 999999, 99999999944217 ] }
-// > web3.eth.getBalance(web3.eth.accounts[1])
+
+// contractInstance.sell(10, {from: web3.eth.accounts[1], value:web3.toWei(10, "ether"), gas:3000000})
+// '0x60a9b567bd11d545933e16ee4c8877978a7e227675d7011514d6500a5dd8b592'
+// contractInstance.buy(10, {from: web3.eth.accounts[2], value:web3.toWei(10, "ether"), gas:3000000})
+// '0xf3b8fe2296c30c640414ea1bfae20fb4feff5dcc4d1f6836ef23a23e5dad5ba6'
+// web3.eth.getBalance(web3.eth.accounts[1])
+// { [String: '109999999999999769386'] s: 1, e: 20, c: [ 1099999, 99999999769386 ] }
+// web3.eth.getBalance(web3.eth.accounts[2])
+// { [String: '89999999999999969254'] s: 1, e: 19, c: [ 899999, 99999999969254 ] }
