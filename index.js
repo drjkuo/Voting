0ccf97dba763b8c3f03b3a5ea0f78b46469e8590
KUO <drjkuo@gmail.com>
2017-11-23 23:18:46 -0800

    ok to trade twice

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 // update API for solar
-abi = JSON.parse('[{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"x","type":"bytes32"}],"name":"bytes32ToString","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"contractOwner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"type":"constructor"}]')
+abi = JSON.parse('[{"constant":false,"inputs":[{"name":"amount","type":"uint256"}],"name":"buy","outputs":[{"name":"bought","type":"uint256"},{"name":"","type":"address"}],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"b","type":"bool"}],"name":"getQueue","outputs":[{"components":[{"name":"amount","type":"uint256"},{"name":"price","type":"uint256"},{"name":"user","type":"address"}],"name":"ret","type":"tuple[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"amount","type":"uint256"}],"name":"sell","outputs":[{"name":"sold","type":"uint256"},{"name":"","type":"address"}],"payable":true,"stateMutability":"payable","type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"value","type":"uint256"},{"indexed":false,"name":"amount","type":"uint256"},{"indexed":false,"name":"price","type":"uint256"}],"name":"PriceCalculated","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"length","type":"uint256"}],"name":"showLength","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"s","type":"string"}],"name":"test","type":"event"}]')
 VotingContract = web3.eth.contract(abi);
 // In your nodejs console, execute contractInstance.address to get the address at which the contract is deployed and change the line below to use your deployed address
-contractInstance =  VotingContract.at('0xb5882d8064351c24916bb089af8f89eae71b03e2');
-candidates = {"0": "candidate-0", "1": "candidate-1", "2": "candidate-2", "3": "candidate-3", "4": "candidate-4"
-}
+contractInstance =  VotingContract.at('0xe2aff68781985a9bf0a726ac7771e388499fc6dd');
+candidates = {"0": "candidate-0", "1": "candidate-1", "2": "candidate-2", "3": "candidate-3", "4": "candidate-4"}
 
 // function voteForCandidate() {
 //   candidateName = $("#candidate").val();
@@ -14,6 +13,14 @@ candidates = {"0": "candidate-0", "1": "candidate-1", "2": "candidate-2", "3": "
 //     $("#" + div_id).html(contractInstance.totalVotesFor.call(candidateName).toString());
 //   });
 // }
+
+function buySolar() {
+  contractInstance.buy(10, {from: web3.eth.accounts[2], value:web3.toWei(10, "ether"), gas:3000000});
+}
+
+function sellSolar() {
+  contractInstance.sell(10, {from: web3.eth.accounts[1], value:web3.toWei(10, "ether"), gas:3000000});
+}
 
 function tradeSolar() {
   contractInstance.buy(10, {from: web3.eth.accounts[2], value:web3.toWei(10, "ether"), gas:3000000});
